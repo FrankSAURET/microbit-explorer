9f07a8b100dfba988a41636ce2af48ae54da58ad
frank4466 <frank.sauret@gmail.com>
2022-05-06 16:22:02 +0200

    suite

---
+++ scripts/build-i18n.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+const fg = require("fast-glob");
+const fs = require("fs-extra");
+const DEFAULT_LOCALE = 'en';
+(async () => {
+    const fallbackMessages = JSON.parse(await fs.readFile(`./locales/${DEFAULT_LOCALE}.json`, 'utf-8'));
+    const files = await fg('./locales/*.json');
+    for (const file of files) {
+        const { name: locale } = path.parse(file);
+        const messages = JSON.parse(await fs.readFile(file, 'utf-8'));
+        Object.keys(fallbackMessages)
+            .forEach((key) => {
+            messages[key] = messages[key] || fallbackMessages[key];
+        });
+        const output = locale === DEFAULT_LOCALE
+            ? './package.nls.json'
+            : `./package.nls.${locale.toLowerCase()}.json`;
+        await fs.writeFile(output, JSON.stringify(messages, null, 2), 'utf-8');
+    }
+})();
+//# sourceMappingURL=build-i18n.js.map
