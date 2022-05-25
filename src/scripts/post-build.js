d2decd16f98ea6d16e12fca5bd412c2c4a59e2ac
frank4466 <frank.sauret@gmail.com>
2022-05-25 16:05:27 +0200

    déplacement locales

--- src/scripts/post-build.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs_extra_1 = require("fs-extra");
-async function run() {
-    await fs_extra_1.default.writeFile('dist/extension.d.ts', 'export * from \'./src/extension\'');
-}
-run();
-//# sourceMappingURL=post-build.js.map
